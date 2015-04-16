d8fe2ac6722c45ff9a7639589ddd2a864da6e14a
周建顺 <zhoujianshun@zhoujianshundeMacBook-Pro.local>
2015-04-16 18:54:32 +0800

    Initial Commit

---
+++ ZJSImagePicker_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJSImagePicker_Demo
+//
+//  Created by 周建顺 on 15/4/16.
+//  Copyright (c) 2015年 周建顺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJSImagePicker_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJSImagePicker_Demo
+//
+//  Created by 周建顺 on 15/4/16.
+//  Copyright (c) 2015年 周建顺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
